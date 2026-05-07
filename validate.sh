8d7231d4c290f16705c22387f16f49f3017d622e
ivanj0rge <ivandcarvalho29@gmail.com>
2026-05-07 12:27:17 +0100

    Validation steps made by a scrip

---
+++ validate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f log.txt ]; then
+  echo "log.txt does not exist"
+  exit 1
+fi
+
+if [ ! -s log.txt ]; then
+  echo "log.txt is empty!"
+  exit 1
+fi
+
+if ! grep -q "Pipeline executed successfully" log.txt; then
+  echo "Expected message not found!"
+  exit 1
+fi
+
+echo "Validation passed!"
+
